03adbb10714c56573572580f59c6c8f9bb9fef7e
HuyNguyen <developer.vanhuy@gmail.com>
2024-01-10 23:15:04 +0700

    [UI] resonsive some ui

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -9,7 +9,7 @@ import Image from '../Image';
 export default function Footer() {
   return (
     <div>
-      <div className='flex flex-wrap gap-6 px-[50px] pb-[55px] max-sm:px-5 max-sm:pb-6 max-sm:pt-3 pt-10 items-center bg-[#faf8f4]'>
+      <div className='flex flex-wrap gap-6 px-[50px] pb-[55px] max-sm:px-5 max-sm:pb-6 max-sm:pt-3 max-md:px-8 max-md:pb-8 max-md:pt-5 pt-10 items-center bg-[#faf8f4]'>
         <div className='grid flex-1 grid-cols-4 mt-12 gap-x-2 gap-y-6 max-lg:grid-cols-2 max-md:hidden'>
           <div>
             <h3 className='font-bold uppercase'>Shop</h3>
--- src/components/Products/index.tsx
+++ src/components/Products/index.tsx
@@ -12,7 +12,7 @@ interface IProps {
 
 export default function Products({ data, title, buttonTitle, view }: IProps) {
   return (
-    <div className='mx-[60px] max-sm:mx-3'>
+    <div className='mx-[60px] max-sm:mx-3 max-lg:mx-6'>
       <div className='flex items-center gap-5 mt-16 mb-8 text-xl max-sm:mt-10 max-sm:mb-4'>
         <h3 className='font-bold ml-[10px]'>{title}</h3>
         {buttonTitle && <Button className='bg-[#ffb4b4] min-w-[120px] font-semibold'>{buttonTitle}</Button>}
