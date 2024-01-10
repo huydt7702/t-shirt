8830ecd23ff6209ece0c86be561c253c128a4a89
HuyNguyen <developer.vanhuy@gmail.com>
2024-01-10 16:13:14 +0700

    [Feature] scroll to top

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,12 +1,31 @@
+import { FaArrowUp } from 'react-icons/fa6';
 import { Outlet } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+
 import Header from 'src/components/Header';
 import TopBar from 'src/components/TopBar';
-import NavBar from '../NavBar';
 import Footer from '../Footer';
+import NavBar from '../NavBar';
 
 export default function Layout() {
+  const [scrollY, setScrollY] = useState(window.scrollY);
+
+  const handleScrollToTop = () => {
+    window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
+  };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrollY(window.scrollY);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [scrollY]);
+
   return (
-    <div>
+    <div className='relative'>
       <div>
         <TopBar />
         <Header />
@@ -18,6 +37,14 @@ export default function Layout() {
       <div>
         <Footer />
       </div>
+      <button
+        className={`fixed bottom-16 right-2 z-[500] flex items-center justify-center w-12 h-12 bg-gradient-to-r from-[#ff7eb9] to-[#ffcc00] rounded-full hover:opacity-80 transition-all ${
+          scrollY >= 1200 ? 'opacity-100' : 'opacity-0 pointer-events-none'
+        }`}
+        onClick={handleScrollToTop}
+      >
+        <FaArrowUp />
+      </button>
     </div>
   );
 }
