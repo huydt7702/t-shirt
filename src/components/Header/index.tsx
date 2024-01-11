303f9c80c16bca00dd1e0af0b984de8b6ed2d82c
HuyNguyen <developer.vanhuy@gmail.com>
2024-01-11 17:00:43 +0700

    [UI] build sidebar cart

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,4 @@
+import Tippy from '@tippyjs/react/headless';
 import { memo, useEffect, useState } from 'react';
 import { BsSearch } from 'react-icons/bs';
 import { HiMiniBars3 } from 'react-icons/hi2';
@@ -6,11 +7,32 @@ import { Link } from 'react-router-dom';
 import { PATH_PUBLIC } from 'src/routes/path';
 import { CartIcon, Logo, UserIcon } from '../Icons';
 import Sidebar from './Sidebar';
+import SidebarCart from './SidebarCart';
 import './styles.css';
+
+interface IUserDropdown {
+  id: number;
+  title: string;
+  path: string;
+}
+
+const userDropdown: IUserDropdown[] = [
+  {
+    id: 1,
+    title: 'Profile',
+    path: '#'
+  },
+  {
+    id: 2,
+    title: 'Logout',
+    path: '#'
+  }
+];
 
 function Header() {
   const [showHeader, setShowHeader] = useState<boolean>(true);
   const [isShowSidebar, setIsShowSidebar] = useState<boolean>(false);
+  const [isShowSidebarCart, setIsShowSidebarCart] = useState<boolean>(false);
 
   useEffect(() => {
     const scrollPosition = 360;
@@ -24,6 +46,24 @@ function Header() {
 
     return () => window.removeEventListener('scroll', handleScroll);
   }, []);
+
+  const renderUserDropdown = () => (
+    <ul
+      className='bg-white shadow-[rgba(0,0,0,0.14)_0px_1px_4px_0px] rounded-md min-w-[140px] overflow-hidden'
+      tabIndex={-1}
+    >
+      {userDropdown.map((item) => (
+        <li key={item.id}>
+          <Link
+            to={item.path}
+            className='block py-2 px-3 font-medium hover:bg-[rgba(0,0,0,0.05)] transition-background'
+          >
+            {item.title}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
 
   return (
     <header
@@ -54,15 +94,22 @@ function Header() {
           <BsSearch className='absolute right-2 top-[50%] translate-y-[-50%]' />
         </div>
         <div className='flex items-center gap-3'>
-          <button>
-            <UserIcon />
-          </button>
-          <button className='relative bottom-[-1px]'>
+          <div className='flex items-center'>
+            <Tippy interactive trigger='click' placement='bottom-start' render={renderUserDropdown}>
+              <button>
+                <UserIcon />
+              </button>
+            </Tippy>
+          </div>
+
+          <button className='relative bottom-[-1px]' onClick={() => setIsShowSidebarCart(true)}>
             <CartIcon />
             <span className='absolute bottom-[-3px] right-[-6px] text-xs bg-[#ffceba] rounded-full w-[16px] h-[16px] flex justify-center items-center'>
               3
             </span>
           </button>
+          <SidebarCart isShowSidebar={isShowSidebarCart} setIsShowSidebar={setIsShowSidebarCart} />
+
           <button className='scale-[130%] lg:hidden' onClick={() => setIsShowSidebar(true)}>
             <HiMiniBars3 />
           </button>
